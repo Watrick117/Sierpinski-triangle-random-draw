0fa95998e7ac040a8df2713a9bf846ae2f6ad6d6
Watrick117 <60557071+Watrick117@users.noreply.github.com>
2021-02-01 19:13:25 -0500

    Update main.py

--- main.py
+++ main.py
@@ -5,35 +5,30 @@
 import pyglet
 from pyglet.gl import *
 from pyglet.window import mouse
+from random import randrange
 
 A = [315,446]
 B = [75,70]
 C = [548,70]
 
-point = A
-
-list_points = [315, 446, 75, 70, 548, 70]
-print(list_points)
+#Creats a list with the first the points of the trinagle
+# [x,y x,y x,y]
+# [A, B, C]
+points = [315,446, 75,70, 548,70, 322,298]
+print(points)
+print(points[-2]) # x axis
+print(points[-1]) # y axis
 
 window = pyglet.window.Window()
 
+# TODO: Finish anchoring to the text is in the top left corner
+# and and displays argparse info
+# TODO: Change title or the GUI
 label = pyglet.text.Label('Sierpinski Triangle Test',
                           font_name='Times New Roman',
                           font_size=10,
                           x=70, y=15,
                           anchor_x='center', anchor_y='center')
-
-#pixels = pyglet.graphics.draw(2, pyglet.gl.GL_POINTS,
-#                    ('v2i', (10, 15, 30, 35)))
-
-def random_number():
-    pass
-
-def fill_list():
-    pass
-
-def draw_pixel():
-    pass
 
 @window.event
 def on_key_press(symbol, modifiers):
@@ -50,8 +45,36 @@ def on_draw():
     label.draw()
 
     # Draws the pixels in the list to the screen
-    total = int(len(list_points)/2)
+    total = int(len(points)/2)
     pyglet.graphics.draw(total, pyglet.gl.GL_POINTS,
-                    ('v2f', (list_points)))
+                    ('v2f', (points)))
 
+def midpoint(one, two):
+    return int((one + two)/2)
+
+def main():
+
+    for i in range(0, 100): #TODO: might need to change to while loop
+
+        #Sets temp to a random number between 0 and 4
+        temp = randrange(0, 5, 2)
+
+        DEBUG = [points[temp],points[temp+1]]
+
+        if DEBUG == A:
+            print('A')
+        elif DEBUG == B:
+            print('B')
+        elif DEBUG == C:
+            print('C')
+
+        DEBUG_X = points[temp]
+        DEBUG_Y = points[temp+1]
+        
+        points.append(midpoint(points[-2],points[temp])) # adds to x axis
+        points.append(midpoint(points[-1],points[temp+1])) # adds to y axis
+
+    print(points)
+
+main()
 pyglet.app.run()
