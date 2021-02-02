65670d0f4a33d5a99816c9f4bb0fcd3d7a2e8a1d
Watrick117 <60557071+Watrick117@users.noreply.github.com>
2021-02-01 22:07:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -13,63 +13,74 @@ from random import randrange
 WIDTH = 1920
 HEIGHT = 1080
 
-#centerCoord = (coord[0]+(coord[2]/2), coord[1]+(coord[3]/2))
-screencenter = (int(WIDTH/2), int(HEIGHT/2))
+MIN = min(WIDTH,HEIGHT)
+MAX = max(WIDTH,HEIGHT)
+MIN10 = int(MIN / 10)
+print(f'{MIN10 = }')
+
+#screencenter = (int(WIDTH/2), int(HEIGHT/2))
 
 #Creats a list with the first the points of the trinagle
 # [x,y x,y x,y]
 # [A, B, C]
 #points = [315,446, 75,70, 548,70, 322,298]
-points = []
-"""points = [(90 * min(WIDTH,HEIGHT)) /100 , (90 * min(WIDTH,HEIGHT)) /100,
- min(WIDTH,HEIGHT) - (90 * min(WIDTH,HEIGHT)) /100 , min(WIDTH,HEIGHT) - (90 * min(WIDTH,HEIGHT)) /100, 
- 548,70, 
- ]"""
+"""points = [
+    int(90 * MIN /100) , int(90 * MIN /100),
+    int(MIN - (90 * MIN /100)) , int(MIN - (90 * MIN /100)), 
+    int(MIN + (10 * MIN /100)) , int(MIN - (10 * MIN /100))
+]"""
+
+points = [
+    MIN - MIN10, MIN - MIN10,
+    MAX - MIN10, 0 + MIN10,
+    0 + MIN10, 0 + MIN10
+]
+
+print('(90 * MIN /100): ' + str((90 * MIN /100)))
+print('(10 * MIN /100): ' + str((10 * MIN /100)))
+
+#points = []
 
 window = Window(width=WIDTH, height=HEIGHT, fullscreen=True)
 
-# TODO: Finish anchoring to the text is in the top left corner
-# and and displays argparse info
 # TODO: Change title or the GUI
 label = pyglet.text.Label('Sierpinski Triangle Test',
                           font_name='Times New Roman',
-                          font_size=10,
-                          x=70, y=15,
-                          anchor_x='center', anchor_y='center')
+                          font_size=20,
+                          x=0, y=HEIGHT,
+                          anchor_x='left', anchor_y='top')
 
 @window.event
 def on_key_press(symbol, modifiers):
     print('A key was pressed')
 
 @window.event
-def on_mouse_press(x, y, button, modifiers):
-    if button == mouse.LEFT:
-        print('x: ' + str(x) + 'y: ' + str(y))
-    if len(points) < 3:
-        points.append(x,y)
-    
-    
-
-@window.event
 def on_draw():
     window.clear()
     label.draw()
 
-    # Draws the pixels in the list to the screen
     total = int(len(points)/2)
     pyglet.graphics.draw(total, pyglet.gl.GL_POINTS,
                     ('v2f', (points)))
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    if button == mouse.LEFT:
+        print('x: ' + str(x) + 'y: ' + str(y))
 
 def midpoint(one, two):
     return int((one + two)/2)
 
 def main():
 
+    #if len(points) > 6:
+
     last_point = (points[-2],points[-1])
-    new_point = (0,0)
 
-    for i in range(0, 100): #TODO: might need to change to while loop
+    #for i in range(0, 100): #TODO: might need to change to while loop
 
+    i = 0
+    while(i < 50000):
         #Sets temp to a random number between 0 and 4
         temp = randrange(0, 5, 2)
 
@@ -85,7 +96,9 @@ def main():
         points.append(new_point[0]) # adds to x axis
         points.append(new_point[1]) # adds to y axis
 
-    print(points)
+        i += 1
+        
+    #print(points)
 
 main()
 pyglet.app.run()
