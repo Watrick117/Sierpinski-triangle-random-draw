21e3fdd64b78f0c53f247ba49541f49855420bc8
Watrick117 <60557071+Watrick117@users.noreply.github.com>
2021-02-02 00:02:20 -0500

    argparse

--- main.py
+++ main.py
@@ -1,40 +1,58 @@
 #!/user/bin/env python3
 # Patrick Woltman
+# https://mathworld.wolfram.com/SierpinskiSieve.html
 # https://www.youtube.com/watch?v=IGlGvSXkRGI
-
-# TODO: argparse
 
 import pyglet
 from pyglet.gl import *
 from pyglet.window import Window
 from pyglet.window import mouse
 from random import randrange
+import argparse
 
-WIDTH = 1920
-HEIGHT = 1080
+my_parser = argparse.ArgumentParser()
+my_parser.add_argument('-s', '--size', action='store', type=int, nargs=2,
+    help="Takes in two arguments that represent the size of the screen.")
+my_parser.add_argument('-i', '--iterations', action='store', type=int, nargs=1,
+    help="How many points will be added to the screen")
+my_parser.add_argument('-f', '--fullscreen', action='store_false',
+    help="Used to turn off fullscreen mode")
+
+# Sanity checks user inputs
+args = my_parser.parse_args()
+
+for x in vars(args):
+    print(f'{getattr(args, x) = }')
+
+if args.size is None:
+    WIDTH = 1920
+    HEIGHT = 1080
+else:
+    WIDTH = args.size[0]
+    HEIGHT = args.size[1]
+
+if args.iterations is None:
+    iterations = 100000
+else:
+    iterations = args.iterations[0]
 
 MIN = min(WIDTH,HEIGHT)
 MAX = max(WIDTH,HEIGHT)
 
 MIN10 = int(MIN / 10)
 
-
-#Creats a list with the first three points of the trinagle
+"""Create a list with the first three points of the triangle,
+based on the size of the screen"""
 points = [
     MIN - MIN10, MIN - MIN10,
     MAX - MIN10, 0 + MIN10,
     0 + MIN10, 0 + MIN10
 ]
 
-print('(90 * MIN /100): ' + str((90 * MIN /100)))
-print('(10 * MIN /100): ' + str((10 * MIN /100)))
+window = Window(width=WIDTH, height=HEIGHT, 
+    caption='Sierpinski Triangle', fullscreen=args.fullscreen)
 
-#points = []
-
-window = Window(width=WIDTH, height=HEIGHT, fullscreen=True)
-
-# TODO: Change title or the GUI
-label = pyglet.text.Label('Sierpinski Triangle Test',
+label = pyglet.text.Label('Sierpinski Triangle',
                           font_name='Times New Roman',
                           font_size=20,
                           x=0, y=HEIGHT,
@@ -66,7 +84,7 @@ def main():
     last_point = (points[-2],points[-1])
 
     i = 0
-    while(i < 2000000):
+    while(i < iterations):
         #Sets temp to a random number between 0 and 4
         temp = randrange(0, 5, 2)
 
